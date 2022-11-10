a447fdc37ab6790f7b353629f3017b78448816eb
Anne Erdtsieck <anne.erdtsieck@topicus.nl>
2022-11-10 17:27:45 +0100

    Example integration test postgres

---
+++ PostgresCicdIntegrationTest/PostgresCicdIntegrationTest/PostgreSqlTest.cs
@@ -0,0 +1,41 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Npgsql;
+using Xunit;
+
+namespace PostgresCicdIntegrationTest;
+
+
+public sealed class PostgreSqlTest : IAsyncLifetime
+{
+    private readonly TestcontainerDatabase _testcontainers = new TestcontainersBuilder<PostgreSqlTestcontainer>()
+        .WithDatabase(new PostgreSqlTestcontainerConfiguration
+        {
+            Database = "db",
+            Username = "postgres",
+            Password = "postgres",
+        })
+        .Build();
+
+    [Fact]
+    public void ExecuteCommand()
+    {
+        using var connection = new NpgsqlConnection(_testcontainers.ConnectionString);
+        using var command = new NpgsqlCommand();
+        connection.Open();
+        command.Connection = connection;
+        command.CommandText = "SELECT 1";
+        command.ExecuteReader();
+    }
+
+    public Task InitializeAsync()
+    {
+        return _testcontainers.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _testcontainers.DisposeAsync().AsTask();
+    }
+}
